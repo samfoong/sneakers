2331359cac573f1dfb35b939b5d4a79ec0df4217
Sam <samfoong88@gmail.com>
2018-05-07 21:38:14 +1000

    Added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products
   resources :charges
+
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
 end
