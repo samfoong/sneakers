853bc9b271cff1de518d5ebdb364b46a0209aa62
Sam <samfoong88@gmail.com>
2018-05-08 15:30:33 +1000

    Add Validations for Users and Listings

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
     belongs_to :user
     mount_uploader :image, ImageUploader
+
+    validates :title, :description, :price, presence: true
+    validates :price, numericality: { greater_than: 0 }
+    validates_presence_of :image
+
 end
