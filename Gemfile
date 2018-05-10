cb72f3e9537d4ff8810c5b0c0ac18587c04a2f6d
Sam <samfoong88@gmail.com>
2018-05-11 02:58:37 +1000

    added lorem ipsum for aboute and contact page

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'dotenv-rails'
 gem 'stripe'
 gem "mini_magick"
 gem 'jquery-rails'
+
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
@@ -47,6 +48,7 @@ group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'rails-erd'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,18 +35,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :medium do
-    process resize_to_fit: [400, 400]
+    process :resize_to_fit => [400, 400]
   end
 
   version :thumb do
-    process resize_to_fit: [200, 200]
+    process :resize_to_fit => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
